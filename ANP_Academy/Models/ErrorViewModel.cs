55d355d95c765f2ca35370664047ba8a9f7b877f
DanielRojasF <87210796+DanielRojasF@users.noreply.github.com>
2024-03-28 19:33:23 -0600

    Commit Menu
    Se agrega la vista del menu de usuario

---
+++ ANP_Academy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ANP_Academy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
