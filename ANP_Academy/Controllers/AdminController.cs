b44b2f1c15d0e12f76bed0e83733df674c31b1f9
Ismael Leon <ileon30419@ufide.ac.cr>
2024-04-06 00:48:27 -0600

    Cambios en la pagina Home y Opciones de Admin
    Se añadio unos cambios dentro del Header del Home, ademas de agregar las opciones de modulos que tiene el admin

---
+++ ANP_Academy/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ANP_Academy.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult DashBoard()
+        {
+            return View();
+        }
+
+   
+    }
+}
--- ANP_Academy/Controllers/HomeController.cs
+++ ANP_Academy/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace ANP_Academy.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
