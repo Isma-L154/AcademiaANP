4e381a0585f3f2b04bf6ad752c283d07833973ef
JoseUrenaO <jauo2810@gmail.com>
2024-04-10 15:53:33 -0600

    Vista clase

---
+++ ANP_Academy/Controllers/ClaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ANP_Academy.Controllers
+{
+    public class ClaseController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
