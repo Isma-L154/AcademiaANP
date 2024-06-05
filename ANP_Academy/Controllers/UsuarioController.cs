d32db19a79c0468f5c913ac8b69748d7e76cb8be
JoseUrenaO <jauo2810@gmail.com>
2024-06-05 16:32:44 -0600

    Sprint 01 GUS-007, 008, 009

--- ANP_Academy/Controllers/UsuarioController.cs
+++ ANP_Academy/Controllers/UsuarioController.cs
@@ -1,11 +1,103 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ANP_Academy.DAL.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace ANP_Academy.Controllers
 {
+    [Authorize]
     public class UsuarioController : Controller
-    {  
-            // Action for the Signup view
-         public IActionResult Signup()
+    {
+        private readonly UserManager<Usuario> _userManager;
+        private readonly AnpdesarrolloContext _dbContext;
+        private readonly SignInManager<Usuario> _signInManager;
+
+        public UsuarioController(UserManager<Usuario> userManager, AnpdesarrolloContext dbContext, SignInManager<Usuario> signInManager)
+        {
+            _userManager = userManager;
+            _dbContext = dbContext;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        public async Task<IActionResult> Edit()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Usuario model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Nombre = model.Nombre;
+            user.PrimApellido = model.PrimApellido;
+
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                // Guardar cambios en el DbContext
+                await _dbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignOutAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Signup()
          {
             return View();
          }
@@ -17,11 +109,6 @@ namespace ANP_Academy.Controllers
         public IActionResult Login()
         {
             return View();
-        }
-
-        public IActionResult EditarPerfil()
-        {
-            return View();
-        }
+        }        
     }
 }
