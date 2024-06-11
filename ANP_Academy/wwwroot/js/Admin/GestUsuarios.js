b94a04056c277fdadc51b257a78270cef2b181ac
Ismael Leon <ileon30419@ufide.ac.cr>
2024-06-10 22:30:13 -0600

    Cambios extras

--- ANP_Academy/wwwroot/js/Admin/GestUsuarios.js
+++ ANP_Academy/wwwroot/js/Admin/GestUsuarios.js
@@ -22,14 +22,16 @@ document.getElementById('BotonBuscar').addEventListener('click', function (event
     event.preventDefault();
     var textoBusqueda = document.getElementById('InputTexto').value.toLowerCase();
     var filas = document.querySelectorAll('#TablaUsuarios tbody tr');
-
-    filas.forEach(function (fila) {
+    filas.forEach(function (fila) { //Iteramos sobre cada una de las filas que en este caso seria las <tr>
         var coincide = false;
-        fila.querySelectorAll('td').forEach(function (celda) {
-            if (celda.textContent.toLowerCase().indexOf(textoBusqueda) > -1) {
+
+        //Buscamos todas las <td> dentro de las filas, cada una de estas van a ser celdas y vamos a iterar sobre ellas
+        fila.querySelectorAll('td').forEach(function (celda) { 
+            if (celda.textContent.toLowerCase().indexOf(textoBusqueda) > -1) { //Convertimos a Lowercase todo para evitar errores y buscamos si el texto proporcionado esta dentro de alguna de estas celdas
                 coincide = true;
             }
         });
+        
         if (coincide) {
             fila.style.display = '';
         } else {
